945fa3f966d9129a713287fa981dea7faab7a09b
David Apakin <apakin.david@gmail.com>
2024-07-09 22:25:11 +0400

    Added mongoose custom validation

--- models/person.js
+++ models/person.js
@@ -19,13 +19,14 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         minLength: 3,
+        required: true,
     },
     number: {
         type: String,
         minLength: 8,
         validate: {
             validator: function(v) {
-                return /\d{3}-\d{8}/.test(v);
+                return /^(\d{2,3})-(\d+)$/.test(v);
             },
             message: props => `${props.value} is not a valid phone number!`
         },
