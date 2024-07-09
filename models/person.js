d5db8611a82a5585f33aea2074dab361df8ee8d7
David Apakin <apakin.david@gmail.com>
2024-07-09 20:54:21 +0400

    Fix .gitignore

--- models/person.js
+++ models/person.js
@@ -20,7 +20,19 @@ const personSchema = new mongoose.Schema({
         type: String,
         minLength: 3,
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{8}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+
+
+    },
 })
 
 personSchema.set('toJSON', {
